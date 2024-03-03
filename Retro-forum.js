db7c9709e519b23077c226f62c7049a746a13859
FARUK2059 <omarfaruk34552@gmail.com>
2024-03-03 21:39:43 +0600

    Discuss show data complat

--- Retro-forum.js
+++ Retro-forum.js
@@ -3,7 +3,7 @@ const loadRetro = async ( ) => {
     const data = await res.json();
     // console.log(data);
     const reTro = data.posts;
-    console.log(reTro);
+    // console.log(reTro);
     displayRetro (reTro);
 
 }
@@ -15,7 +15,7 @@ const displayRetro = (reTro) => {
         const reTroDiscuss = document.getElementById('discussContainer');
 
         reTro.forEach ( reTroAll => {
-            // console.log(reTroAll);
+            console.log(reTroAll);
 
             // step 2 : Creat a new div
             const discussCard = document.createElement('div');
@@ -29,16 +29,16 @@ const displayRetro = (reTro) => {
                                 <div> 
                                     <div class="indicator">
                                         <span class="indicator-item badge badge-secondary bg-red-800 "></span> 
-                                        <div class="grid w-32 h-32 bg-base-300 place-items-center rounded-xl"><img src="./images/joinforum.png" alt=""></div>
+                                        <div class="grid w-32 h-32 bg-base-300 place-items-center rounded-xl">${reTroAll.image}</div>
                                     </div>
                                 </div>
                                 <div class="grid lg:col-span-4">
                                     <div class="flex text-[#12132DCC]">
-                                        <h3># Music</h3>
-                                        <h3 class="ml-10"> Author : Awlad Hossian</h3>
+                                        <h3>${reTroAll.category}</h3>
+                                        <h3 class="ml-10">${reTroAll.author.name}</h3>
                                     </div>
-                                    <h1 class="text-[#12132D] font-bold text-xl">10 Kids Unaware of Their Halloween Costume</h1>
-                                    <p class="text-[#12132D99]">It’s one thing to subject yourself to ha Halloween costume mishap because, hey that’s your prerogative</p>
+                                    <h1 class="text-[#12132D] font-bold text-xl"> ${reTroAll.title}</h1>
+                                    <p class="text-[#12132D99]"> ${reTroAll.description} </p>
                                     <br>
                                     <hr style="border-top: dashed 1px;" />
                                     <br>
@@ -46,15 +46,15 @@ const displayRetro = (reTro) => {
                                         <div class="flex gap-8">
                                             <div class="flex">
                                                 <span class="flex"><img src="./images/Masseg icon.png" alt=""></span>
-                                                <p class="ml-4">560</p>
+                                                <p class="ml-4"> ${reTroAll.comment_count} </p>
                                             </div>
                                             <div class="flex">
                                                 <span class="flex mr-4"><img src="./images/Group 16.png" alt=""></span>
-                                                <p class="">1,568</p>
+                                                <p class=""> ${reTroAll.view_count} </p>
                                             </div>
                                             <div class="flex">
                                                 <span class="flex mr-4"><img src="./images/Time.png" alt=""></span>
-                                                <p class="">5 min</p>
+                                                <p class=""> ${reTroAll.posted_time} </p>
                                             </div>
                                         </div>
                                         <div>
